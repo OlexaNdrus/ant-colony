16942ddbf70259aba65c10580ea573f35496803a
Alex <abysho666@gmail.com>
2021-01-15 16:19:23 +0200

    Last comm

--- example.py
+++ example.py
@@ -1,6 +1,18 @@
 import numpy as np
+import xlrd
 
 from ant_colony import AntColony
+
+workbook = xlrd.open_workbook('Дані.xlsx')
+ws = workbook.sheet_by_index(0)
+cost_mat=[]
+for col in range(2, ws.ncols):
+    cost_mat.append([])
+    for row in range(1, ws.nrows):
+        if ws.cell_value(row, col)==0:
+            cost_mat[col - 2].append(np.inf)
+        else:
+            cost_mat[col-2].append(int(ws.cell_value(row, col)))
 
 distances = np.array([[np.inf, 2, 2, 5, 7],
                       [2, np.inf, 4, 8, 2],
@@ -8,7 +20,9 @@ distances = np.array([[np.inf, 2, 2, 5, 7],
                       [5, 8, 1, np.inf, 2],
                       [7, 2, 3, 2, np.inf]])
 
-ant_colony = AntColony(distances, 1, 1, 100, 0.95, alpha=1, beta=1)
+distances = np.array(cost_mat)
+
+ant_colony = AntColony(distances, 50, 50, 100, 0.2, alpha=1, beta=1)
 shortest_path = ant_colony.run()
-print "shorted_path: {}".format(shortest_path)
+print ("shorted_path: {}".format(shortest_path))
 
